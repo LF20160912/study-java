6147e875435963ca372c3b2e19897bc406aa2166
LF20160912 <2576731178@qq.com>
2022-02-17 22:43:46 +0800

    add new project

---
+++ zuulGateway/src/test/java/com/mk/study/cloudapigateway/CloudApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.study.cloudapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApiGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
